170ec05798081b8942b7f0dfaf2f4aad2e56ac0a
Yah-ir-M <122851161+Yah-ir-M@users.noreply.github.com>
2025-01-29 17:26:40 -0600

    new

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log('Hello Instructor, What do you like to do on your free time?');
+
 document.write("Howdy");
+
---
+++ variableassignment/app.js
@@ -0,0 +1,17 @@
+{
+    var x = 5;
+}
+// this is the global variable
+
+{
+    let x = 10;
+}
+// this is the block variable
+
+//string
+let color = "blue";
+
+//object
+const person = {firstName:"Captain", lastName:"America"};
+
+console.log("let");
